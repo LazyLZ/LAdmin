fb60d3d4dafc2f5c2e5c9625e287b82021be88b3
LZ <laizhi727@126.com>
2018-11-28 00:16:35 +0800

    Create Project
    Signed-off-by: LZ <laizhi727@126.com>

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    "semi": [2, "never"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "comma-dangle": [2, "only-multiline"],
+    "brace-style": [2, "stroustrup"],
+    "object-curly-spacing": [2, "never"]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
