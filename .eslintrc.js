4cacd617d6c96648967ffea76e7e93f4d9a27fda
LZ <laizhi727@126.com>
2018-12-06 11:44:06 +0800

    更新组件文档显示
    Signed-off-by: LZ <laizhi727@126.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   ],
   rules: {
     'no-self-compare': [0],
+    'vue/no-use-v-if-with-v-for': [0],
     "semi": [2, "never"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
